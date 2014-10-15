53c684bd1723f093398948ed19e02855ba317699
kfl62 <kfl62g@gmail.com>
2014-10-15 17:32:00 +0300

    controllers: controllere de baza issue #6

---
+++ system/controllers/assets.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+module Cnd
+  # #Cnd::Assets module
+  # ##Description
+  # ##Scope
+  # @todo document this module
+  module Assets
+    # #Sass/Compass Handler
+    class Stylesheets < Sinatra::Base
+      set :static, true
+      register  CompassInitializer
+
+      # @todo Document this method
+      get '/stylesheets/:name.css' do
+        content_type 'text/css', :charset => 'utf-8'
+        scss params[:name].to_sym, Compass.sass_engine_options
+      end
+    end
+    # #Coffeescript Handler
+    class Javascripts < Sinatra::Base
+      set :static, true
+      use Rack::Coffee,
+        root: Cnd.assets,
+        cache_compile: true
+    end
+  end
+end
