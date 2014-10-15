74d8f3bea944198bcdb7b079c6c39e44e5d8c4e7
kfl62 <kfl62g@gmail.com>
2014-10-15 17:17:10 +0300

    config: config files issue #5

---
+++ config/boot.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+
+CND_ROOT = File.expand_path('../',File.dirname(__FILE__))
+CND_ENV  = ENV['RACK_ENV'] || 'development'
+# Load our dependencies
+require 'rubygems' unless defined?(Gem)
+require 'bundler/setup'
+require 'ostruct'
+require 'yaml'
+Bundler.require(:default, CND_ENV)
+# Haml :hyphenate_data_attrs defaults to true since v.4.0.0
+# @todo rewrite code, mostly js, and 118n related
+Haml::Options.defaults[:hyphenate_data_attrs] = false
+# Load libraries
+Dir.glob(File.join(CND_ROOT, 'lib','**','*.rb')).each{|r| require r}
+# Load configurations
+Dir.glob(File.join(CND_ROOT, 'config','**','*.rb')).each{|r| require r}
+# Initialize path for Trst
+Dir['system/*'].each do |dir|
+  dir = File.join(File.expand_path('..',File.dirname(__FILE__)),dir)
+  $LOAD_PATH.unshift(dir) unless $LOAD_PATH.include?(dir)
+end
+# Add path for translations and default language
+require "i18n/backend/fallbacks"
+I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+I18n.load_path += Dir.glob(File.join(CND_ROOT, 'system','i18n','**','*.yml'))
+I18n.enforce_available_locales = false
+I18n.default_locale = :ro
+I18n.fallbacks[:ro]=[:ro,:en]
+# Sintra default settings
+Sinatra::Base.set(:root, CND_ROOT)
+Sinatra::Base.set(:cdn,  true)
+Sinatra::Base.mime_type(:otf, ' font/otf')
+Sinatra::Base.mime_type(:ttf,  'font/ttf')
+Sinatra::Base.mime_type(:eot,  'application/vnd.ms-fontobject')
+Sinatra::Base.mime_type(:svg,  'image/svg+xml')
+Sinatra::Base.mime_type(:woff, 'font/woff')
---
+++ config/cnd.rb
@@ -0,0 +1,74 @@
+# encoding: utf-8
+# ## Scope
+#   A module wich includes four {http://www.sinatrarb.com/ Sinatra::Base} sub classes
+#   {Cnd::Public}, {Cnd::System}, {Cnd::Assets}  and {Cnd::Utils}
+#   ...
+# @todo document this model
+module Cnd
+  @@opt = OpenStruct.new
+  class << self
+    # @todo
+    def opt
+      @@opt
+    end
+    # @todo
+    def opt=(v)
+      @@opt = v
+    end
+    # @todo
+    def root
+      opt.root || CND_ROOT
+    end
+    # @todo
+    def env
+      opt.env || CND_ENV
+    end
+    # @todo
+    def views
+      opt.views || File.join(CND_ROOT,'system','views')
+    end
+    # @todo
+    def assets
+      opt.assets || File.join(CND_ROOT,'system','assets')
+    end
+    # @todo
+    def models
+      opt.models || File.join(CND_ROOT,'system','models')
+    end
+    # @todo
+    def firm
+      file = YAML.load_file File.join(CND_ROOT,'config','firm.yml')
+      OpenStruct.new file['firm']
+    end
+  end
+  # @todo document this method
+  def self.server
+    Rack::Builder.new do
+      use Rack::Session::Cookie,
+        expire_after: 12*60*60,
+        secret: '14743354a2e77700a28a2d0aa8bebee57c42452713d115232b9abafc5dae9a12'
+      use Rack::Rewrite do
+        rewrite %r{^/\w{2}/utils}, '/utils'
+        rewrite %r{^/\w{2}/sys},  '/sys'
+        rewrite %r{^/\w{2}/},      '/'
+      end
+
+      map '/utils' do
+        run Utils
+      end
+      map '/sys' do
+        run System
+      end
+      map '/' do
+        run Public
+      end
+    end
+  end
+  autoload :System,               'system'
+  autoload :Public,               Cnd.firm.public['controller']
+  autoload :Utils,                'utils'
+  autoload :Assets,               'assets'
+  Dir.glob(File.join(models,'*.rb')).each do |m|
+    require m
+  end
+end
